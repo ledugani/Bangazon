2f8fcde313eeb5082de52ab3107f84882d7354be
Tom Dugan <tdugan85@gmail.com>
2018-09-08 13:03:18 -0500

    everything works, just missing the hyphens

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Bangazon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string:");
+            Console.Write(">>");
+            var input = Console.ReadLine();
+
+            var counter = 0;
+            
+            foreach(var letter in input)
+            {
+                if (counter == 0)
+                {
+                    Console.Write(letter.ToString().ToUpper() + "-");
+                }
+                else
+                {
+                    for (int i = 0; i <= counter; i++) {
+                        if (i == 0) {
+                            Console.Write(letter.ToString().ToUpper());
+                        } 
+                        else
+                        {
+                            Console.Write(letter);
+                        }
+
+                        Console.Write("-");
+                    }
+                }
+                
+                counter++;
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Press enter to quit.");
+            Console.ReadLine();
+        }
+    }
+}
